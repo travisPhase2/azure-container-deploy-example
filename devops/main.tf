178e6e7b9e787c07f4c302d36d0a443e2f45d342
Travis Musall <travism@phase2online.com>
2024-12-05 12:30:40 -0500

    updates

--- devops/main.tf
+++ devops/main.tf
@@ -2,60 +2,77 @@ resource "random_pet" "rg_name" {
     prefix = var.resource_group_name_prefix
 }
 
-resource "random_string" "container_name" {
-    length = 25
-    lower = true
-    upper = false
-    special = false
-}
-
 resource "azurerm_resource_group" "rg" {
     name = random_pet.rg_name.id
     location = var.resource_group_location
 }
 
 resource "azurerm_user_assigned_identity" "container_identity" {
-  name = "container-app-identity"
-  resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
+    name = "container-app-identity"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
 }
 
 resource "azurerm_container_registry" "acr" {
-    name                = "TravContainerRegistry"
+    name                = "travcontainerregistry"
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
     sku                 = "Basic"
-    admin_enabled       = true
+    admin_enabled       = false
 }
 
-resource "azurerm_role_assignment" "acr_pull_role" {
-  scope = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id = azurerm_user_assigned_identity.container_identity.principal_id
+resource "azurerm_role_assignment" "acr_pull" {
+    scope                = azurerm_container_registry.acr.id
+    role_definition_name = "AcrPull"
+    principal_id         = azurerm_user_assigned_identity.container_identity.principal_id
 }
 
-resource "azurerm_container_group" "container" {
-    name = "${var.container_group_name_prefix}-${random_string.container_name.result}"
-    location = azurerm_resource_group.rg.location
+resource "azurerm_user_assigned_identity" "deployment_identity" {
+    name                = "deployment-identity"
     resource_group_name = azurerm_resource_group.rg.name
-    ip_address_type = "Public"
-    os_type = "Linux"
-    restart_policy = var.restart_policy
+    location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+    scope                = azurerm_container_registry.acr.id
+    role_definition_name = "AcrPull"
+    principal_id         = azurerm_user_assigned_identity.deployment_identity.principal_id
+}
+
+resource "azurerm_container_app_environment" "example" {
+    name                = "trav-app-environment"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_container_app" "example" {
+    name                          = "trav-app"
+    container_app_environment_id  = azurerm_container_app_environment.example.id
+    resource_group_name           = azurerm_resource_group.rg.name
+    revision_mode                 = "Single"
+    location                      = azurerm_resource_group.rg.location
 
     identity {
-      type = "UserAssigned"
-      identity_ids = [azurerm_user_assigned_identity.container_identity.id]
+        type = "UserAssigned"
+        identity_ids = [azurerm_user_assigned_identity.deployment_identity.id]
     }
 
-    container {
-        name = "${var.container_name_prefix}${random_string.container_name.result}"
-        image = "${var.image}"
-        cpu = var.cpu_cores
-        memory = var.memory_in_gb
+    ingress {
+        external_enabled = true
+        target_port     = 8000
 
-        ports {
-          port = var.port
-          protocol = "TCP"
+        traffic_weight {
+            latest_revision = true
+            percentage     = 100
+        }
+    }
+
+    template {
+        container {
+            name   = "app"
+            image  = "${var.image}"
+            cpu    = 0.5
+            memory = "1Gi"
         }
     }
 }

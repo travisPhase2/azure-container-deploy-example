a31d957bbddff5fb6333158a89876f064b46106c
Travis Musall <travism@phase2online.com>
2024-12-05 12:32:01 -0500

    fix

--- devops/main.tf
+++ devops/main.tf
@@ -24,19 +24,13 @@ resource "azurerm_container_registry" "acr" {
 resource "azurerm_role_assignment" "acr_pull" {
     scope                = azurerm_container_registry.acr.id
     role_definition_name = "AcrPull"
-    principal_id         = azurerm_user_assigned_identity.container_identity.principal_id
+    principal_id         = azurerm_user_assigned_identity.deployment_identity.principal_id
 }
 
 resource "azurerm_user_assigned_identity" "deployment_identity" {
     name                = "deployment-identity"
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
-}
-
-resource "azurerm_role_assignment" "acr_pull" {
-    scope                = azurerm_container_registry.acr.id
-    role_definition_name = "AcrPull"
-    principal_id         = azurerm_user_assigned_identity.deployment_identity.principal_id
 }
 
 resource "azurerm_container_app_environment" "example" {

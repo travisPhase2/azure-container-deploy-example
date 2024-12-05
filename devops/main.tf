842d5ff6351d0cb4c5782b89555f318be9878608
Travis Musall <travism@phase2online.com>
2024-12-05 13:34:24 -0500

    test

--- devops/main.tf
+++ devops/main.tf
@@ -1,65 +1,49 @@
-resource "random_pet" "rg_name" {
-    prefix = var.resource_group_name_prefix
+resource "azurerm_resource_provider_registration" "app" {
+    name = "Microsoft.App"
 }
 
-resource "azurerm_resource_group" "rg" {
-    name = random_pet.rg_name.id
-    location = var.resource_group_location
-}
-
-resource "azurerm_user_assigned_identity" "container_identity" {
-    name = "container-app-identity"
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-}
-
-resource "azurerm_container_registry" "acr" {
-    name                = "travcontainerregistry"
-    resource_group_name = azurerm_resource_group.rg.name
-    location            = azurerm_resource_group.rg.location
-    sku                 = "Basic"
-    admin_enabled       = false
-}
-
-resource "azurerm_role_assignment" "acr_pull" {
-    scope                = azurerm_container_registry.acr.id
-    role_definition_name = "AcrPull"
-    principal_id         = azurerm_user_assigned_identity.deployment_identity.principal_id
-}
-
-resource "azurerm_user_assigned_identity" "deployment_identity" {
-    name                = "deployment-identity"
-    resource_group_name = azurerm_resource_group.rg.name
-    location            = azurerm_resource_group.rg.location
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = var.resource_group_location
 }
 
 resource "azurerm_log_analytics_workspace" "example" {
-  name                = "trav-analytics-ws"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = "acctest-01"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
 
 resource "azurerm_container_app_environment" "example" {
-  name                       = "trav-container-app-env"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
+  name                       = "Example-Environment"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
 }
 
 resource "azurerm_container_app" "example" {
-  name                         = "trav-app"
+  name                         = "example-app"
   container_app_environment_id = azurerm_container_app_environment.example.id
-  resource_group_name          = azurerm_resource_group.rg.name
+  resource_group_name          = azurerm_resource_group.example.name
   revision_mode                = "Single"
 
   template {
     container {
-      name   = "trav-container-app"
+      name   = "examplecontainerapp"
       image  = "mcr.microsoft.com/k8se/quickstart:latest"
       cpu    = 0.25
       memory = "0.5Gi"
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port = 80
+
+    traffic_weight {
+      percentage = 100
+      latest_revision = true
     }
   }
 }
--- devops/providers.tf
+++ devops/providers.tf
@@ -5,19 +5,9 @@ terraform {
         source = "hashicorp/azurerm"
         version = "~>3.0"
     }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.0"
-    }
-    random = {
-        source = "hashicorp/random"
-        version = "~>3.0"
-    }
   }
 }
 
 provider "azurerm" {
     features { }
 }
-
-provider "azuread" { }

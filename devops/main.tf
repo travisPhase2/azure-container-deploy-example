b3277031e5b127ffb21ea4f3f67759c56d769604
Travis Musall <travism@phase2online.com>
2024-12-06 13:46:00 -0500

    updates

--- devops/main.tf
+++ devops/main.tf
@@ -1,31 +1,51 @@
-resource "azurerm_resource_provider_registration" "app" {
-    name = "Microsoft.App"
-}
+# resource "azurerm_resource_provider_registration" "app" {
+#     name = "Microsoft.App"
+# }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "rg" {
+  name     = "Compellier"
   location = var.resource_group_location
 }
 
-resource "azurerm_log_analytics_workspace" "example" {
-  name                = "acctest-01"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+# logging analytics workspace
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "Compellier-LAW"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
 
-resource "azurerm_container_app_environment" "example" {
-  name                       = "Example-Environment"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+# container registry
+resource "azurerm_container_registry" "cr" {
+  name                = "CompellierContainerRegistry"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
-resource "azurerm_container_app" "example" {
-  name                         = "example-app"
-  container_app_environment_id = azurerm_container_app_environment.example.id
-  resource_group_name          = azurerm_resource_group.example.name
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.cr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_container_registry.cr.identity[0].principal_id
+}
+
+resource "azurerm_container_app_environment" "cae" {
+  name                       = "Compellier-CAE"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+}
+
+resource "azurerm_container_app" "ca" {
+  name                         = "containerapp"
+  container_app_environment_id = azurerm_container_app_environment.cae.id
+  resource_group_name          = azurerm_resource_group.rg.name
   revision_mode                = "Single"
 
   template {
@@ -39,33 +59,17 @@ resource "azurerm_container_app" "example" {
 
   ingress {
     external_enabled = true
-    target_port = 80
+    target_port      = 80
 
     traffic_weight {
-      percentage = 100
+      # 100% of traffic to the latest revision of container
+      percentage      = 100
       latest_revision = true
     }
   }
 
+  # required for the container app to pull from the container registry
   identity {
     type = "SystemAssigned"
   }
-}
-
-resource "azurerm_container_registry" "example" {
-  name                = "exampleappregistry"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku                 = "Basic"
-  admin_enabled       = true
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
-
-resource "azurerm_role_assignment" "example_acr_pull" {
-  scope                = azurerm_container_registry.example.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_container_registry.example.identity[0].principal_id
 }
--- devops/variables.tf
+++ devops/variables.tf
@@ -1,5 +1,5 @@
 variable "resource_group_location" {
   type        = string
-  default     = "eastus"
+  default     = "southcentralus"
   description = "Location for all resources."
 }
